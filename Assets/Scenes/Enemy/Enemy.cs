e5806fc0b212a04517f6aaf2a51f22d0ee8e6e42
Keyrune <spyro-99@mail.ru>
2021-03-22 11:58:18 +0300

    Added basic enemy

---
+++ Assets/Scenes/Enemy/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GreenBird greenBird = collision.collider.GetComponent<GreenBird>();
+        if (greenBird != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
